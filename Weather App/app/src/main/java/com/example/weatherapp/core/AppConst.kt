67cb8c28d52c515ea8ecaf5d56ca1b9512d02f5d
Eslam Elsayed <eslimelseyd@gmail.com>
2025-03-22 01:03:16 +0200

    add app const

---
+++ Weather App/app/src/main/java/com/example/weatherapp/core/AppConst.kt
@@ -0,0 +1,10 @@
+package com.example.weatherapp.core
+
+object AppConst {
+    const val BASE_URL = "https://api.openweathermap.org/"
+    const val API_KEY = "8d3ddb2b35dc4e9af6b35fe480e54769"
+    const val IMAGE_URL = "https://openweathermap.org/img/wn/"
+    const val IMAGE_EXE = "@2x.png"
+    const val TEMP_DEGREE = "°"
+
+}
