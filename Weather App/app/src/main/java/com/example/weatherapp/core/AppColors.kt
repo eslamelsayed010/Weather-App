151c669de8f515df5818b93abc79d0eb7f75b995
Eslam Elsayed <eslimelseyd@gmail.com>
2025-03-24 00:24:37 +0200

    add some color

--- Weather App/app/src/main/java/com/example/weatherapp/core/AppColors.kt
+++ Weather App/app/src/main/java/com/example/weatherapp/core/AppColors.kt
@@ -3,6 +3,7 @@ package com.example.weatherapp.core
 import androidx.compose.ui.graphics.Color
 
 object AppColors {
+    //Gray
     val Gray50 = Color(0xFFFAFAFA)
     val Gray100 = Color(0xFFF5F5F5)
     val Gray200 = Color(0xFFEEEEEE)
@@ -13,11 +14,19 @@ object AppColors {
     val Gray700 = Color(0xFF616161)
     val Gray800 = Color(0xFF424242)
     val Gray900 = Color(0xFF212121)
-    //BK color
+
+    //BK
     val SunnyBK = Color(0xFF305d3f)
     val RainyBK = Color(0xff144761)
     val CloudsBK = Color(0xff5daee0)
     val MistBK = Color(0xffdbc5a6)
     val ThunderstormBK = Color(0xff305d3f)
     val SnowBK = Color(0xff2b454e)
+
+    //Btn Nav Bar
+    val SettingNav = Color(0xff0d0129)
+
+    //Cat
+    val IconCat = Color(0xff94d693)
+    val TitleCat = Color(0xff0091e3)
 }
